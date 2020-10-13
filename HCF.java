a0e3f8b29bdebb36bcaeae1f2350c99434ed57a5
HAREESH <53457844+hareesh-r@users.noreply.github.com>
2020-10-13 23:38:16 +0530

    Update HCF.java

--- HCF.java
+++ HCF.java
@@ -9,7 +9,7 @@ public class HCF {
         System.out.println("HCF of " + num1 + " " + num2 + " is " + hcf);
         sc.close();
     }
-
+    /*Algorithm Used is Eucledian Algorithm*/
     public static int GCD(int num1, int num2) {
         if (num1 == 0)
             return num2;
