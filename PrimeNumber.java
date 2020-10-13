e5db9ece1a0d47ea4fab2d5ab3d1beea2c9835e7
HAREESH <53457844+hareesh-r@users.noreply.github.com>
2020-10-13 23:36:53 +0530

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-/* Methosd Used Sieve Of Eratosthenes  */
+/* Method Used Sieve Of Eratosthenes  */
 public class PrimeNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
